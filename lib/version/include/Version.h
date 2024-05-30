98dd7b60ab797ddadcb4bc7cf043bef5d1c28da1
OctSquid <pqvs6k96@s.okayama-u.ac.jp>
2024-05-30 11:08:28 +0900

    🎨 Change: バージョン番号をマクロに変更

--- lib/version/include/Version.h
+++ lib/version/include/Version.h
@@ -3,9 +3,8 @@
 
 #include <stdint.h>
 
-// バージョン番号を定義します
-const uint8_t VERSION_MAJOR = 1;
-const uint8_t VERSION_MINOR = 0;
-const uint8_t  VERSION_PATCH = 0;
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
 
 #endif // VERSION_H
